474a038c36f8c1f1aa294ea4ff1103cdb16e60bc
soyeon12345 <didthduswkd2@naver.com>
2019-03-16 08:39:50 +0900

    03.16

--- app/src/main/java/com/jsl/capstonedesign/activity/Download.java
+++ app/src/main/java/com/jsl/capstonedesign/activity/Download.java
@@ -1,7 +1,12 @@
 package com.jsl.capstonedesign.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jsl.capstonedesign.R;
 
@@ -11,5 +16,31 @@ public class Download extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+
+
+        TextView tx_logo =(TextView) findViewById(R.id.tx_logo);
+        tx_logo.setOnClickListener(new Button.OnClickListener()
+                                   {
+                                       @Override
+                                       public void onClick(View v)
+                                       {
+                                           Intent intent = new Intent(getApplicationContext(), Home.class);
+                                           startActivity(intent);
+                                       }
+                                   }
+        );
+
+        ImageView img_search = (ImageView)findViewById(R.id.img_search);
+        img_search.setOnClickListener(new Button.OnClickListener()
+                                      {
+                                          @Override
+                                          public void onClick(View v)
+                                          {
+                                              Intent intent = new Intent(getApplicationContext(), Search_result.class);
+                                              startActivity(intent);
+                                          }
+                                      }
+
+        );
     }
 }
