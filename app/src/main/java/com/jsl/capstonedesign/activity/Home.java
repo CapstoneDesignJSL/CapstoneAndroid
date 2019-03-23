ad4000d353a394d762a78cff2e8e7a7f1c894bad
이현규\Hyun Gyu <dlgusrb0813@gmail.com>
2019-03-23 12:51:19 +0900

    login구현

--- app/src/main/java/com/jsl/capstonedesign/activity/Home.java
+++ app/src/main/java/com/jsl/capstonedesign/activity/Home.java
@@ -2,19 +2,62 @@ package com.jsl.capstonedesign.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
 import com.jsl.capstonedesign.R;
 
 public class Home extends AppCompatActivity {
+    //나중에 삭제할것  **수정
+    private ImageView test;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //*************************************************로그인관련************
+        test = findViewById(R.id.img_cart);  //로그아웃 테스트버튼 나중에 버튼 새로 구현 **수정
+        mAuth = FirebaseAuth.getInstance();
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        //*************************************************로그인관련************
+
+        test.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.e("알람","리스너");
+                signOut();
+            }
+        });
 
         ImageView img_israel = (ImageView)findViewById(R.id.img_israel);
         img_israel.setOnClickListener(new Button.OnClickListener()
@@ -68,6 +111,45 @@ public class Home extends AppCompatActivity {
                                     }
 
         );
+
+    }
+
+    public void signOut() {
+        mGoogleApiClient.connect();
+        mGoogleApiClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+                mAuth.signOut();
+                if(mGoogleApiClient.isConnected()) {
+                    Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+                            if (status.isSuccess()) {
+                                Log.v("알림", "로그아웃 성공");
+                                setResult(1);
+                            } else {
+                                setResult(0);
+                            }
+
+                            Intent intent = new Intent(getApplicationContext(),Login.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onConnectionSuspended(int i) {
+
+                Log.v("알림", "Google API Client Connection Suspended");
+
+                setResult(-1);
+
+                finish();
+
+            }
+
+        });
 
     }
 }
--- app/src/main/java/com/jsl/capstonedesign/activity/MainActivity.java
+++ app/src/main/java/com/jsl/capstonedesign/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.jsl.capstonedesign.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,13 +11,15 @@ import android.widget.ImageView;
 import com.jsl.capstonedesign.R;
 
 public class MainActivity extends AppCompatActivity {
+    final private String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//로고 클릭시 로그인 화면뜸
+        //로고 클릭시 로그인 화면뜸
 
+        Log.e(TAG,"onCreate in Main");
         ImageView logo = (ImageView)findViewById(R.id.img_in);
         logo.setOnClickListener(new Button.OnClickListener()
                                         {
