ccff85a1205188c4e641e91dc888dde1ba8a0cd2
soyeon12345 <didthduswkd2@naver.com>
2019-03-27 00:09:10 +0900

    33

--- app/src/main/java/com/jsl/capstonedesign/activity/Home.java
+++ app/src/main/java/com/jsl/capstonedesign/activity/Home.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -20,9 +23,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.jsl.capstonedesign.R;
 
 public class Home extends AppCompatActivity {
+
+
+
     //나중에 삭제할것  **수정
     private ImageView test;
-
     private FirebaseAuth mAuth;
     private GoogleApiClient mGoogleApiClient;
 
@@ -30,6 +35,27 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+//        프래그먼트
+        final ImageView user = (ImageView) findViewById(R.id.img_user);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
+
+        user.setOnClickListener(new View.OnClickListener()
+                                      {
+                                          @Override
+                                          public void onClick(View view)
+                                          {
+                                              notice.setVisibility(View.GONE);
+                                              FragmentManager fragmentManager = getSupportFragmentManager();
+                                              FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                                              fragmentTransaction.replace(R.id.fragment, new MenuFragment());
+                                              fragmentTransaction.commit();
+                                          }
+                                      }
+
+        );
+
 
         //*************************************************로그인관련************
         test = findViewById(R.id.img_cart);  //로그아웃 테스트버튼 나중에 버튼 새로 구현 **수정
@@ -100,18 +126,6 @@ public class Home extends AppCompatActivity {
 
         );
 
-        ImageView img_user = (ImageView)findViewById(R.id.img_user);
-        img_user.setOnClickListener(new Button.OnClickListener()
-                                    {
-                                        @Override
-                                        public void onClick(View v)
-                                        {
-                                            Intent intent = new Intent(getApplicationContext(), User.class);
-                                            startActivity(intent);
-                                        }
-                                    }
-
-        );
 
     }
 
