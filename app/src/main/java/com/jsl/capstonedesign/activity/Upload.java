b5f11327b71765a537cb817e540fb078a27fe495
이현규\Hyun Gyu <dlgusrb0813@gmail.com>
2019-05-15 07:13:29 +0900

    upload

--- app/src/main/java/com/jsl/capstonedesign/activity/Upload.java
+++ app/src/main/java/com/jsl/capstonedesign/activity/Upload.java
@@ -252,4 +252,6 @@ public class Upload extends Activity {
 
 
     }
+
+
 }
--- app/src/main/java/com/jsl/capstonedesign/activity/UploadPopup.java
+++ app/src/main/java/com/jsl/capstonedesign/activity/UploadPopup.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,14 +17,38 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.jsl.capstonedesign.R;
+import com.jsl.capstonedesign.activity.Retrofit.ApiService;
+import com.jsl.capstonedesign.activity.recyclerview.Data;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.xml.transform.Result;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class UploadPopup extends Activity implements OnClickListener {
-    private static final String TAG = "SYSTEM";
+    private static final String TAG = UploadPopup.class.getSimpleName();
     private Context mContext = null;
     private final int imgWidth = 320;
     private final int imgHeight = 372;
+    private File img;
+    private String imgPath;
+    private Bitmap bm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +59,16 @@ public class UploadPopup extends Activity implements OnClickListener {
         /** 전송메시지 */
         Intent i = getIntent();
         Bundle extras = i.getExtras();
-        String imgPath = extras.getString("filename");
+        imgPath = extras.getString("filename");
+        Log.e(TAG, "img path = "+imgPath);
+
+        img = new File(imgPath);
 
         /** 완성된 이미지 */
         BitmapFactory.Options bfo = new BitmapFactory.Options();
         bfo.inSampleSize = 2;
         ImageView iv = findViewById(R.id.sell_image);
-        Bitmap bm = BitmapFactory.decodeFile(imgPath, bfo);
+        bm = BitmapFactory.decodeFile(imgPath, bfo);
         Bitmap resized = Bitmap.createScaledBitmap(bm, imgWidth, imgHeight, true);
         iv.setImageBitmap(resized);
 
@@ -52,16 +82,21 @@ public class UploadPopup extends Activity implements OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_upload_cancel:
-                Toast.makeText(getApplicationContext(),"취소",Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(),"취소",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(mContext, Upload.class);
                 startActivity(intent);
                 break;
             case R.id.btn_upload_register:
                 EditText nameEdit = findViewById(R.id.sell_name);
                 EditText priceEdit = findViewById(R.id.sell_price);
-                String pricename = nameEdit.getText().toString();
+                String name = nameEdit.getText().toString();
                 String pricevalue = priceEdit.getText().toString();
-                Toast.makeText(mContext, pricename+pricevalue, Toast.LENGTH_SHORT).show();
+//                float price = Float.parseFloat(pricevalue);
+                pricevalue="0";
+                upload(name, pricevalue);
+
+
+//                Toast.makeText(mContext, pricename+pricevalue, Toast.LENGTH_SHORT).show();
                 break;
         }
     }
@@ -71,5 +106,66 @@ public class UploadPopup extends Activity implements OnClickListener {
         $bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream) ;
         byte[] byteArray = stream.toByteArray() ;
         return byteArray ;
+    }
+
+    public void upload(String name, String price_str){
+
+        String str_img =  getStringFromBitmap(bm);
+        String email = "dlgusrb0813@gmail.com";
+
+        float price = Float.parseFloat(price_str);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ApiService.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        final ApiService apiService = retrofit.create(ApiService.class);
+        Uri url = Uri.parse(imgPath);
+        // create RequestBody instance from file
+        RequestBody requestFile =
+                RequestBody.create(
+                        MediaType.parse(
+                      //   getContentResolver().getType(url))
+                        "multipart/form-data")
+                    ,
+                        img
+                );
+
+
+        // MultipartBody.Part is used to send also the actual file name
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("picture", img.getName(), requestFile);
+
+
+        //Part of GET
+        Call<RequestBody> res = apiService.uploadImg(email,name,str_img,price);
+
+        res.enqueue(new Callback<RequestBody>() {
+            @Override
+            public void onResponse(Call<RequestBody> call, Response<RequestBody> response) {
+                Log.e(TAG, "onResponse in upload");
+
+
+            }
+            @Override
+            public void onFailure(Call<RequestBody> call, Throwable t) {
+                Log.e(TAG, "onFailure in upload");
+
+
+
+            }
+        });
+
+
+    }
+
+    private String getStringFromBitmap(Bitmap bitmapPicture) {
+        String encodedImage;
+        ByteArrayOutputStream byteArrayBitmapStream = new ByteArrayOutputStream();
+        bitmapPicture.compress(Bitmap.CompressFormat.PNG, 100, byteArrayBitmapStream);
+        byte[] b = byteArrayBitmapStream.toByteArray();
+        encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encodedImage;
     }
 }
